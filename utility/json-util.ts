96b0550228c6962e19a09ec232e5b6a29ce9a9f9
Kent <>
2025-10-08 15:47:33 +0800

    feat: json util

---
+++ utility/json-util.ts
@@ -0,0 +1,35 @@
+/**
+ * json工具
+ */
+export class JsonUtil {
+    /**
+     * json轉map
+     * @param src json字串
+     */
+    static toMap<TK, TV>(src: string): Map<TK, TV> {
+        const exchange = function(json: {}): Map<TK, TV> {
+            let res = new Map<TK, TV>();
+
+            for (const key in json) {
+                res.set(<TK>key, <TV>json[key]);
+            }
+
+            return res;
+        }
+
+        return exchange(JSON.parse(src));
+    }
+
+    /**
+     * map轉json字串
+     */
+    static fromMap(src: Map<any, any>): string {
+        const exchange = function(src: Map<any, any>): {} {
+            let res = {};
+            src.forEach((value, key) => res[key] = value);
+            return res;
+        }
+
+        return JSON.stringify(exchange(src));
+    }
+}
